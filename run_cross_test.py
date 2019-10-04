fb402e89d0aaf4126fba77ed7fff69c325f78087
basten1 <1542094@uni-wuppertal.de>
2019-10-04 17:11:39 +0200

    Added a pythonscript to create a Plot of the usage of all exits and anotherone to run the test and create the plot

--- run_cross_test.py
+++ run_cross_test.py
@@ -2,8 +2,8 @@ from exit_usage import plot_me
 import os
 import subprocess
 
-
-jpscore = "/home/guido/Programme/JuPedSim/jpscore/build/bin/jpscore"
+# Path to jpscore
+jpscore = "/.../jpscore/build/bin/jpscore"
 
 test_dir = "{}/cross_ini.xml".format(os.getcwd())
 
