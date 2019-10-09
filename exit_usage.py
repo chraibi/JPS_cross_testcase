383499344e1b81a1a8e3df017ce47f4e554c4059
Mohcine Chraibi <m.chraibi@fz-juelich.de>
2019-10-09 13:42:27 +0200

    formatting changes and jpscore as input

--- exit_usage.py
+++ exit_usage.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
 class flow_exit:
-    def __init__(self,num):
+    def __init__(self, num):
         self.path = "flow_exit_id_{}_cross.txt".format(num)
         self.data = self.get_data()
 
@@ -23,14 +23,15 @@ class flow_exit:
 def plot_me(plotname):
     for i in range(8):
         flow = flow_exit(i)
-        plt.plot(flow.data,label="Exit number {}".format(i))
+        plt.plot(flow.data, label="Exit number {}".format(i))
 
     plt.legend()
     plt.grid()
-    plt.xlim(0,100)
-    plt.ylim(0,200)
+    plt.xlim(0, 100)
+    plt.ylim(0, 200)
     plt.savefig(plotname)
     #plt.show()
 
 if __name__ == "__main__":
-    plot_me("exit_usage.svg")
+    print("plotting from within main --> exit_usage.png")
+    plot_me("exit_usage.png")
--- run_cross_test.py
+++ run_cross_test.py
@@ -1,12 +1,22 @@
-from exit_usage import plot_me
 import os
 import subprocess
+import datetime
+import time
+from exit_usage import plot_me
+import sys
+
+if len(sys.argv) == 1:
+    sys.exit("usage: %s full_path_to_jpscore"%sys.argv[0])
 
 # Path to jpscore
-jpscore = "/.../jpscore/build/bin/jpscore"
+jpscore = sys.argv[1]
 
 test_dir = "{}/cross_ini.xml".format(os.getcwd())
 
 
-subprocess.run([jpscore,test_dir])
-plot_me('exit_usage.svg')
+subprocess.run([jpscore, test_dir])
+ts = time.time()
+st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d-%H%M%S')
+figname = '%s.png'%st
+print("output: %s"%figname)
+plot_me(figname)
